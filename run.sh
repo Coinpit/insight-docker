e7ac85b811e9b076f09792bda652a3bc5eae1063
Nirmal Gupta <nirmalk.gupta@gmail.com>
2015-12-13 20:25:21 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -13,6 +13,7 @@ fi
 for net in livenet testnet
 do
   if [ ! -d ${INSIGHT_DATA}/$net ]
+  then
     cp -r /opt/insight/$net ${INSIGHT_DATA}
     cp -r /opt/insight/node_modules ${INSIGHT_DATA}/$net
   fi
